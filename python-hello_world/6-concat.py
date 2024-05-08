7275a7b228d6299cab8ec2b18baf9e2874a5bd14
Ibrux11 <ibraluunge@gmail.com>
2024-05-08 16:36:05 +0200

    tst

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-concatenated_str = str1 + " " + str2
-print(f"Welcome to {concatenated_str}!")
+str3 = str1 + " " + str2
+print(f"Welcome to {str3}!")
