ed3d4c1033b58959e195cce1cda4408718f22833
Ibrux11 <ibraluunge@gmail.com>
2024-06-20 15:41:31 +0200

    com

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,5 +1,2 @@
 -- that inserts a new row in the table first_table (database hbtn_0c_0)
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT UNIQUE,
-    name VARCHAR(256)
-);
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
