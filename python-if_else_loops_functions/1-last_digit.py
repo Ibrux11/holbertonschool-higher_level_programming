ba1646c4a2da2f468dc8d16df08ebea0d2260edd
Ibrux11 <ibraluunge@gmail.com>
2024-09-04 14:44:15 +0200

    add task 2

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+trunks =abs(number) % 10
+isagi = 1 if number >= 0 else -1
+trunks *= isagi
 if number > 5:
-    print(number, "and is greater than 5")
-if number == 5:
-    print(number, "and is 0")
-if number < 5:
-    print(number, "and is less than 6 and not 0")
+    print(f"Last digit of {number} is {trunks} and is greater than 5")
+elif number == 0:
+    print(f"Last digit of {number} is {trunks} and is 0")
+elif number < 5:
+    print(f"Last digit of {number} is {trunks} and is less than 6 and not 0")
