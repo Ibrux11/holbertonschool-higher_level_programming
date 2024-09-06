e2f508f940ae1e7358d05f8e962a8a505ebaba5c
Ibrux11 <ibraluunge@gmail.com>
2024-09-06 11:12:56 +0200

    add task 6/7/8/9

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for isagi in range(10):
-    for lacazette in range( isagi + 1, 10):
+    for lacazette in range(isagi + 1, 10):
         if isagi == 8 and lacazette == 9:
             print(f"{isagi}{lacazette}".format(isagi, lacazette))
         else:
