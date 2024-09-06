77c75a315e31c520c7ec78cbfc575e7a6a22d498
Ibrux11 <ibraluunge@gmail.com>
2024-09-06 23:19:38 +0200

    add all task 0

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from add_0 import add
+if __name__ == "__main__":
+    goku = 1
+    vegeta = 2
+    result = add(goku, vegeta)
+    print(f"{goku} + {vegeta} = {result}")
---
+++ python-import_modules/0-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(goku, vegeta):
+    """My addition function
+
+    Args:
+        a
+        gok: first integer
+        vegeta: second integer
+
+    Returns:
+        Return value. goku + vegeta
+    """
+    return (goku + vegeta)
