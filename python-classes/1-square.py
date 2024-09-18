792d2b90d99b94e09cacf96f59c95b57f702e350
Ibrux11 <ibraluunge@gmail.com>
2024-09-18 10:23:14 +0200

    add docu task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Define a class square"""
+
 class Square:
     def __init__(self, size):
+        """Initializies a new object with the specified size
+        
+        Args:
+            size: Size of the object.
+        """
         self.__size = size
