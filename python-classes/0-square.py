1d01fc5cfe9dbd4027a961e99418b447e282920d
Ibrux11 <ibraluunge@gmail.com>
2024-09-18 10:10:38 +0200

    add docu task0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""Define a class square"""
+
+
+
 class Square:
+    """
+    This class represents a Square.
+
+    It didnt have attributes or methods currently.
+    """
     pass
--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,5 +4,9 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size =size
+            raise ValueError("size must be >=0")
+        self.__size = size
+
+    def area(self):
+        
+        return self.__size ** 2
