ffe93c9d3fe59b8a9b06e0a5d23634b341ee211d
Ibrux11 <ibraluunge@gmail.com>
2024-09-20 23:07:51 +0200

    add task 0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+
+"""
+Module: rectangle
+Defines a Rectangle class
+"""
 class Rectangle:
     """Rectangle class"""
     def __init__(self):
+        """Initializes a new Rectangle instance."""
         pass
