57fd74a381b31bb3500ee019dd767281c4306200
Ibrux11 <ibraluunge@gmail.com>
2024-09-26 14:48:14 +0200

    add task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
-
+"""
+Class for geometry operations.
+"""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """Class Rectangle that inherits from BaseGeometry"""
+
     def __init__(self, width, height):
+        """Instantiation of width and height with validation"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("heigt", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Returns the area of the rectangle.
+
+        Uses the formula: width * height.
+        """
+        return self.__width * self.__height
