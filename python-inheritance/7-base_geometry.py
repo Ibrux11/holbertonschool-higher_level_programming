44426cca91e64491f6e159983ab89ac576335e06
Ibrux11 <ibraluunge@gmail.com>
2024-09-26 14:28:37 +0200

    add

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -3,5 +3,24 @@
 
 
 class BaseGeometry:
+    """BaseGeometry class with public instance methods for area and integer validation."""
+    
     def area(self):
+        """Raises an Exception as area is not implemented."""
         raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """Validates that value is an integer greater than 0.
+
+        Args:
+            name (str): Name of the parameter.
+            value (int): Value of the parameter to be validated.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
+class Rectangle(BaseGeometry):
+    """Class Rectangle that inherits from BaseGeometry"""
 
+    def __init__(self, width, height):
+        """Instantiation of width and height with validation"""
 
+        self.integer_validator("width", width)
+        self.integer_validator("heigt", height)
 
-
-
-class Rectangle:
+        self.__width = width
+        self.__height = height
