670a5ba909e255917bccc2b408b43b384907e966
Ibrux11 <ibraluunge@gmail.com>
2024-09-26 09:35:03 +0200

    add all task

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+""""""
+
 
 def inherits_from(obj, a_class):
-    
+    """
+    Check if an object is an instance of, or if the object is an instance 
+    of a class that inherited from, the specified class.
+
+    Parameters:
+    obj: Any type
+        The object to be checked.
+    a_class: type
+        The class to compare the object against.
+
+    Returns:
+    bool
+        True if the object is an instance of a_class or an instance
+        of a subclass of a_class, otherwise False.
+    """
     return isinstance(obj, a_class) and type(obj) is not a_class
---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Class for geometry operations.
+    """
+
+    def area(self):
+        """
+        Public instance method that raises an exception indicating
+        that the area method is not implemented.
+        """
+
+        raise Exception("area() is not implemented")
