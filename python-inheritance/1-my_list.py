b9ff9da717a2290507a66fab2a19a93fbafc5616
Ibrux11 <ibraluunge@gmail.com>
2024-09-27 23:45:43 +0200

    add

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""
+A subclass of list that includes a method to print the list in sorted order.
+"""
+
 
 class MyList(list):
+    """
+    Prints the list in sorted (ascending) order.
+    """
+
     def print_sorted(self):
         """
-        Prints the list in sorted (ascending) order without modifying the original list.
-        
-        :Return: None"""
+        The original list is not modified.
+        """
         print(sorted(self))
