242d49969dde0b151ae8d3b20144f906fa059ee4
Ibrux11 <ibraluunge@gmail.com>
2024-10-03 15:24:10 +0200

    aDD

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
-
 def read_file(filename=""):
+    """
+    Lit le contenu d'un fichier et l'affiche sur la sortie standard.
+
+    Args:
+        filename (str): Le chemin du fichier à lire. Par défaut, le nom du fichier est une chaîne vide.
+    
+    Description:
+        Cette fonction ouvre un fichier encodé en UTF-8 et affiche son contenu
+        sur la sortie standard (généralement la console). Si aucun nom de fichier
+        n'est fourni, elle ne fera rien. La fonction lit tout le contenu du fichier
+        et l'affiche sans ajout de saut de ligne à la fin.
+    """
     with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
