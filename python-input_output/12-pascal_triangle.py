10100f298e686564840bb7379bf1ebbf6369f365
Ibrux11 <ibraluunge@gmail.com>
2024-10-04 00:02:37 +0200

    add

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 """
-Module to create a Pascal's Triangle without any helps
-that's gonna be difficult because I don't know what a
-Pascal's triangle is.
+Pascal's Triangle
 """
 
 
 def pascal_triangle(n):
     """
-    Return a list of integers representing the Pascal's
-    triangle of "n"
+    Returns a list of lists of integers representing
+    the Pascal's triangle of n.
     """
-
-    list_pascal_triangle = list()
-
-    index = 0
-
     if n <= 0:
-        return list_pascal_triangle
-    else:
-        for index in range(n):
-            return (1)
+        return []
+
+    triangle = [[1]]
+    for index in range(1, n):
+        row = [1]
+        for index2 in range(1, index):
+            row.append(triangle[index-1][index2-1] + triangle[index-1][index2])
+        row.append(1)
+        triangle.append(row)
+    return triangle
